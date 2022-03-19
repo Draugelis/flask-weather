af337dcba1475d77f817e16b3fbe6dddf11049fc
Draugelis <viktoras.draugelis@gmail.com>
2022-03-20 00:03:33 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import requests
+
+app 	= Flask(__name__)
+
+@app.route('/')
+def home():
+	ip	= request.remote_addr
+	if ip == "127.0.0.1":
+		location = "Vilnius"
+	else:
+		location_request 	= requests.get(f"http://ip-api.com/json/{ip}")
+		location_data		= location_request.json()
+		location 			= location_data['City']
+
+	weather_request = requests.get(f"https://weatherdbi.herokuapp.com/data/weather/{location}")
+	weather_data 	= weather_request.json()
+	temp  			= weather_data["currentConditions"]["temp"]["c"]
+	icon 			= weather_data["currentConditions"]["iconURL"]
+
+	data = {
+		"location"	: location,
+		"temp" 		: temp,
+		"icon"		: icon
+	}
+
+	return render_template('index.html', data=data)
